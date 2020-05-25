1f168cc85440707cc23dc881e256d46e1b79ce76
Shin <shin_2261@hotmail.com>
2020-05-25 22:17:50 +0100

    Initial Commit

--- script.py
+++ script.py
@@ -6,13 +6,13 @@ import requests
 
 name = input("Your Name? ")
 print("Hellp,", name)
-# print(sys.executable)
+print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
 
 
-# r = requests.get("http://coreyms.com")
-# print(r.status_code)
+r = requests.get("http://coreyms.com")
+print(r.status_code)
