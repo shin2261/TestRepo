e79f763f65defb79e90290d0730ce47f790c3e0b
Shin <shin_2261@hotmail.com>
2020-05-25 22:20:10 +0100

    Remove some code

--- script.py
+++ script.py
@@ -1,17 +1,4 @@
-import math
-import os
-import sys
-
 import requests
-
-name = input("Your Name? ")
-print("Hellp,", name)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("http://coreyms.com")
